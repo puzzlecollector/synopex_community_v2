4c03d8a7fa67071a2e8c96970429649de01aec3f
puzzlecollector <puzzlecollector1123581321@gmail.com>
2024-05-09 10:26:28 +0900

    base local prod settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,9 +14,9 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# settings.py
+# base.py
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
